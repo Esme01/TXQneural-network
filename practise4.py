2892dc751fbca5e41103e3135ab8dbc2e0d843af
Esme01 <TXQ01020304@163.com>
2019-09-27 11:49:16 +0800

    Create practise4.py
    debug network

---
+++ practise4.py
@@ -0,0 +1,21 @@
+import math
+import numpy as np
+
+
+def sigmoid(z):
+    return 1/(1+pow(math.e, -z))
+
+
+    def __init__(self, sizes):
+        self.sizes = sizes
+        w_list = []
+        for i in range(len(sizes)-1):
+            w_list.append([np.random.randn(sizes[i+1], sizes[i])])
+
+        self.n = n
+        self.m = m
+        self.weight = np.random.randn(m, n)
+        self.biase = np.random.randn(m, 1)
+
+    def __call__(self, x):
+        return sigmoid(np.matmul(self.weight, x)+self.biase)
